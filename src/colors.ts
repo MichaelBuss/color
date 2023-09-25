a739cd9fd008f8aa1e87b4bfd8ac66f6649bdcec
Michael Buss Rønne <man@trackunit.com>
2023-09-25 15:32:01 +0200

    Add colors routes and some styling

--- src/colors.ts
+++ src/colors.ts
@@ -1,2 +1,26 @@
+import { cva } from "class-variance-authority";
 export const colors = ["red", "blue", "green", "yellow", "purple", "pink"] as const;
 export type Color = (typeof colors)[number];
+
+export const cvaColor = cva([], {
+  variants: {
+    srgb: {
+      red: "bg-srgb-red",
+      green: "bg-srgb-green",
+      blue: "bg-srgb-blue",
+      yellow: "bg-srgb-yellow",
+      cyan: "bg-srgb-cyan",
+      purple: "bg-srgb-purple",
+      pink: "bg-srgb-pink",
+    },
+    p3: {
+      red: "bg-p3-red",
+      green: "bg-p3-green",
+      blue: "bg-p3-blue",
+      yellow: "bg-p3-yellow",
+      gray: "bg-p3-cyan",
+      purple: "bg-p3-purple",
+      pink: "bg-p3-pink",
+    },
+  },
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			"gridTemplateColumns": {
+				"responsive-sm": "repeat(auto-fit, minmax(16ch, 1fr));",
+				"responsive-md": "repeat(auto-fit, minmax(24ch, 1fr));",
+				"responsive-lg": "repeat(auto-fit, minmax(32ch, 1fr));",
+			},
+			"fontSize": {
+				"responsive-title": "clamp(2rem, 6vw, 10rem)",
+			},
+			"colors": {
+				"srgb-red": "color(srgb 100% 0% 0% / 100%)",
+				"srgb-green": "color(srgb 0% 100% 0% / 100%)",
+				"srgb-blue": "color(srgb 0% 0% 100% / 100%)",
+				"srgb-yellow": "color(srgb 100% 100% 0% / 100%)",
+				"srgb-cyan": "color(srgb 0% 100% 100% / 100%)",
+				"srgb-purple": "color(srgb 50% 0% 50% / 100%)",
+				"srgb-pink": "color(srgb 100% 0% 50% / 100%)",
+				"p3-red": "color(display-p3 100% 0% 0% / 100%)",
+				"p3-green": "color(display-p3 0% 100% 0% / 100%)",
+				"p3-blue": "color(display-p3 0% 0% 100% / 100%)",
+				"p3-yellow": "color(display-p3 100% 100% 0% / 100%)",
+				"p3-cyan": "color(display-p3 0% 100% 100% / 100%)",
+				"p3-purple": "color(display-p3 50% 0% 50% / 100%)",
+				"p3-pink": "color(display-p3 100% 0% 50% / 100%)",
+			}
+		},
 	},
 	plugins: [],
 }
